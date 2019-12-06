cda05bffcce4e27b94b712c7e86744605a864a7c
Fraser Speirs <fraser@speirs.org>
2019-12-06 22:45:44 +0000

    Got Context Menu disabling working

--- DocsDisabler/lockdown.js
+++ DocsDisabler/lockdown.js
@@ -21,25 +21,57 @@ function removeElement(e) {
   console.log("Deleted: " + e);
 }
 
+function removeBySelector(s) {
+	console.log("Hiding by selector: " + s);
+	var element = document.querySelector(s);
+	element.style.display = 'none';
+	console.log("Hid by selector:" + s);
+}
+
+function saveLockdownStatus() {
+	chrome.storage.local.set({'priorLockdown': "1"}, function() {
+		console.log("Saved lockdown status")
+	});
+}
+
+
+function interceptContextMenu() {
+	$(".kix-zoomdocumentplugin-inner").contextmenu(function() {
+		return false;
+	});
+}
+
 function lockdown() {
-  var elements = ["docs-tools-menu", "docs-insert-menu", "docs-help-menu", "docs-extensions-menu", "spellGrammarCheckButton", "insertImageButton", "insertCommentButton", "insertLinkButton"];
-      for(var i=0; i < elements.length; i++) {
-        removeElement(elements[i]);
-  
-      }
+	// These elements should always be hidden
+	var general_elements = ["docs-tools-menu", 
+						  "docs-help-menu",
+						  "docs-extensions-menu",
+						  "insertImageButton",
+						  "insertCommentButton",
+						  "insertLinkButton"];
+				  
+	// These elements should only be hidden if spelling is off.
+	var spelling_elements = ["spellGrammarCheckButton"];
+	var insert_elements = ["docs-insert-menu"];
 
-      var buttons = document.querySelector("div.docs-titlebar-buttons");
-      buttons.style.display = 'none';
+	var elements_to_remove = general_elements.concat(spelling_elements).concat(insert_elements);
 
-      var explore = document.querySelector(".docs-explore-widget");
-      explore.style.display = 'none';
+	for(var i=0; i < elements_to_remove.length; i++) {
+		removeElement(elements_to_remove[i]);
+	}
 
-      var exploreSidebar = document.querySelector("div.docs-explore-sidebar");
-      exploreSidebar.style.display = 'none';
+	// Remove elements by querySelector
+	var query_selector_elements = ["div.docs-titlebar-buttons",
+							   ".docs-explore-widget",
+							   "div.docs-explore-sidebar"];
 
-      chrome.storage.local.set({'priorLockdown': "1"}, function() {
-        console.log("Saved lockdown status")
-      });
+	for (var i=0; i < query_selector_elements.length; i++) {
+		removeBySelector(query_selector_elements[i]);
+	}
+
+	interceptContextMenu();
+	
+	saveLockdownStatus();
 }
 
 chrome.runtime.onMessage.addListener(
--- DocsDisabler/popup.js
+++ DocsDisabler/popup.js
@@ -2,6 +2,9 @@ chrome.tabs.onUpdated.addListener(
   function(tabId, changeInfo, tab) {
     // read changeInfo data
     if (changeInfo.status == 'complete' && tab.active) {
+		chrome.tabs.executeScript(null, { file: "jquery.js" }, function() {
+			chrome.tabs.executeScript(null, { file: "content.js" });
+		});
       // url has changed; do something here
       // like send message to content script
       chrome.tabs.sendMessage( tabId, {
@@ -13,13 +16,18 @@ chrome.tabs.onUpdated.addListener(
 );
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-  // Send a message to the active tab
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
-  });
+  //lock_page();
 });
 
 chrome.webNavigation.onDOMContentLoaded.addListener(function() {
   console.log("popup webcontent loaded");
 });
+
+function lock_page() {
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    	var activeTab = tabs[0];
+    	chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+  	});
+}
+
+//document.getElementById('save').addEventListener('click', save_options);
